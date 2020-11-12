157e28262392262d9348c1228e71fd72b474c5c7
IIArmII <73084630+IIArmII@users.noreply.github.com>
2020-11-12 17:25:05 +0300

    upload

---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+function aboutToggle() {
+    var toggle = document.getElementById("about-angle");
+    var about = document.getElementById("about");
+    if (toggle.className.indexOf("fa-angle-right") >= 0) {
+        toggle.className = "fas fa-angle-down";
+        about.classList.add("active");
+    } else {
+        toggle.className = "fas fa-angle-right";
+        about.classList.remove("active");
+    }
+}
+
+function imgBoxClose(it) {
+	let block = it.parentNode.parentNode;
+	block.style.visibility = 'hidden';
+	block.style.opacity = '0'
+}
+
+var showBox = document.querySelector("#showBox");
+var showBoxImg = showBox.querySelector("img")
+
+var imgs = document.getElementsByClassName("showImg");
+for (let i=0; i<imgs.length; i++) {
+	imgs[i].addEventListener("click", function () {
+		let img = imgs[i].parentNode.querySelector("img");
+		showBoxImg.setAttribute("src", img.getAttribute("src"));
+		showBox.style.visibility = 'visible';
+		showBox.style.opacity = '1'
+	})
+}
